de0785abcf072e4902fdedc781be1600d0536e9a
Pugolovkin Evgeniy <47895577+peg-ib@users.noreply.github.com>
2019-12-25 05:37:19 +0300

    Add files via upload

---
+++ boost_filesystem_.cpp
@@ -0,0 +1,168 @@
+﻿#include <iostream>
+#include <boost/filesystem.hpp>
+#include <string>
+struct finance
+{
+	std::string broker;
+	std::string account;
+	int date;
+	int number;
+	finance() = default;
+	finance(std::string broker, std::string account, int date, int number)
+	{
+		this->broker = broker;
+		this->account = account;
+		this->date = date;
+		this->number = number;
+	}
+	finance(boost::filesystem::path& p)
+	{
+		broker = p.parent_path().filename().string();//Директория 
+		std::string number = "";
+		std::string filename = p.filename().string();//Имя файла
+		for (int i = 8; i < 16; i++)
+			number += filename[i];
+		account = number;
+		number = " ";
+		for (int i = 17; i < 25; i++)
+			number += filename[i];
+		date = atoi(number.c_str());
+	}
+};
+//ib balance_00001234_20181001.txt
+bool check_filename(boost::filesystem::path& p)
+{
+	std::string filename = p.filename().string();
+	std::string type = "";
+	if (filename.length() == 29)
+	{
+		for (int i = 0; i < 8; i++)
+			type += filename[i];
+		if (type == "balance_")
+		{
+			if (filename[16] == '_')
+			{
+				if (filename[21] <= '1')//Месяц 
+				{
+					if (filename[21] == '1' && filename[22] <= '2')//Месяц
+					{
+						if (filename[23] <= '3')//День
+						{
+							if (filename[23] == '3' && filename[24] > '1')//День
+							{
+								return false;
+							}
+							else
+							{
+								if (p.extension() == ".txt")
+								{
+									return true;
+								}
+								else
+									return false;
+							}
+						}
+						else
+							return false;
+					}
+					else
+						return false;
+				}
+				else
+					return false;
+			}
+			else
+				return false;
+		}
+		else
+			return false;
+	}
+	else
+		return false;
+}
+void search(const boost::filesystem::path& p, std::vector<finance>& files)
+{
+	boost::filesystem::path _path;
+	if (exists(p))//Определяет,существует ли файл или директория по указанному пути
+	{
+		for (const boost::filesystem::directory_entry& x : boost::filesystem::directory_iterator{ p })
+		{
+			if (boost::filesystem::is_regular_file(x))
+			{
+				_path = x.path();
+				if (check_filename(_path))
+				{
+					std::cout << _path.parent_path().filename() << ' ' << _path.filename() << std::endl;
+					files.push_back(_path);
+				}
+			}
+			else
+			{
+				if (boost::filesystem::is_directory(x))
+				{
+					_path = x.path();
+					search(_path, files);
+				}
+			}
+		}
+	}
+	else
+	{
+		std::cout << p << "does not exist" << std::endl;
+	}
+}
+void print_finance(std::vector<finance> files)
+{
+	std::cout << std::endl;
+	for (int i = 0; i < files.size(); i++)
+	{
+		std::cout << "broker:" << files[i].broker << ' ' << "account:" << files[i].account << ' ' << "files:" << files[i].number << ' ' << "lastdate:" << files[i].date << std::endl;
+	}
+}
+void sorting(std::vector<finance>files)
+{
+	std::vector<finance> new_files;
+	int number;
+	double lastdate;
+	std::string account = "";
+	std::string broker = "";
+	for (int i = 0; i < files.size(); i++)
+	{
+		if ((files[i].account != account) || (files[i].broker != broker))
+		{
+			number = 0;
+			lastdate = files[i].date;
+			for (int j = 0; j < files.size(); j++)
+			{
+				if (files[i].broker == files[j].broker)
+				{
+					if (files[i].account == files[j].account)
+					{
+						number++;
+						if (files[j].date >= lastdate)
+						{
+							lastdate = files[j].date;
+						}
+					}
+				}
+			}
+			finance fin(files[i].broker, files[i].account, lastdate, number);
+			new_files.push_back(fin);
+		}
+		broker = files[i].broker;
+		account = files[i].account;
+	}
+	print_finance(new_files);
+}
+int main()
+{
+	const boost::filesystem::path& p = "D:\\ftp";
+	std::vector<finance> files;
+	search(p, files);
+	sorting(files);
+	return 0;
+}
+
+
+
+
---
+++ stack_.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+#include <exception>
+template <typename T>
+class stack
+{
+private:
+	struct Node //Элемент стека
+	{
+		T data;
+		Node* next;//Указатель на следующий элемент стека
+		Node()
+		{
+			data = NULL;
+			next = NULL;
+		}
+		Node(T data)
+		{
+			this->data = data;
+		}
+		Node(T data, Node* next)
+		{
+			this->data = data;
+			this->next = next;
+		}
+	};
+	Node* top;//Верхний элемент стека
+	int size;//Размер стека
+public:
+	stack()
+	{
+		top = NULL;
+		size = 0;
+	}
+	~stack() {}
+	template <typename ... Args>
+	void push_emplace(Args&&... value)
+	{
+		if (top != NULL)
+		{
+			Node* temp = new Node(T(std::forward<Args>(value)...), top);
+			top = temp;
+		}
+		else
+			top = new Node(T(std::forward<Args>(value)...));
+		size++;
+	}
+	void push(T&& value)
+	{
+		if (top != NULL)
+		{
+			Node* temp = new Node(std::move(value), top);
+			top = temp;
+		}
+		else
+			top = new Node(std::move(value));
+		size++;
+	}
+	void push(const T& value)
+	{
+		if (top != NULL)
+		{
+			Node* temp = new Node(value, top);
+			top = temp;
+		}
+		else
+			top = new Node(value);
+		size++;
+	}
+	void pop()
+	{
+		try
+		{
+			if (top == NULL)
+				throw std::out_of_range("Error: Out of range");
+			Node* temp = top;
+			top = top->next;
+			delete temp;
+			size--;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	const T& head() const
+	{
+		try
+		{
+			if (top == NULL)
+				throw std::out_of_range("Error: Out of range");
+			return top->data;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	void print()
+	{
+		Node* temp = top;
+		while (size > 0)
+		{
+			std::cout << temp->data << std::endl;
+			if (size > 1)
+				temp = temp->next;
+			size--;
+		}
+		std::cout << std::endl;
+	}
+};
+int main()
+{
+	stack <int> Stack;
+	Stack.push(1);
+	Stack.push(2);
+	Stack.push(3);
+	Stack.print();
+	std::cout << "head:" << std::endl;
+	std::cout << Stack.head() << std::endl;
+	Stack.pop();
+	std::cout << std::endl << "delete_element." << std::endl;
+	std::cout << "head:" << std::endl;
+	std::cout << Stack.head() << std::endl;
+	Stack.push_emplace<int>(4);
+	std::cout << std::endl << "push_element." << std::endl;
+	std::cout << "head:" << std::endl;
+	std::cout << Stack.head() << std::endl;
+	return 0;
+}
